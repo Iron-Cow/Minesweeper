1d2a7c36f33fca4344219c3485a123116c93dfce
Iron-Cow <uangeji@gmail.com>
2019-11-27 21:46:21 +0200

    Empty fields massive open + Flag + GameOver

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ if __name__ == "__main__":
     run = True
     pygame.init()
 
-    # rect = Cell(200, 200, 50, (255, 255, 0))
     game_cell = Cell(0, 0, 50, (255, 0, 0))  # example of the cell
     current_field = FieldCoordinates(nx=20, ny=10, exapmle_cell=game_cell).get_field()
 
     window = pygame.display.set_mode(size=(1+len(current_field[0])*game_cell.get_w(),
                                            1+len(current_field)*game_cell.get_w()))
-
-    for _ in range(10):
+    # Bomb filling
+    for _ in range(20):
         while True:
             x = randint(0, len(current_field[0])-1)
             y = randint(0, len(current_field)-1)
@@ -33,12 +32,13 @@ if __name__ == "__main__":
                            grid_color=(100, 255, 5),
                            game_cell=game_cell,
                            field=current_field)
+
     while run:
-        # window.fill((0, 0, 0))
+        window.fill((0, 0, 0))
         game_field.draw_grid(window)
         game_field.draw_cells(window)
 
-        run = event_manager.check_events()
-        # rect.draw(window)
+        run = event_manager.check_events(window)
 
         pygame.display.update()
+
--- models.py
+++ models.py
@@ -30,12 +30,28 @@ class Cell(RectField):
         self.__bomb = True
 
     def draw(self, surface):
-        font = pygame.font.Font("freesansbold.ttf", 14)
-        nearby_bombs = font.render(str(self.__label), True, (0, 255, 0))
-        surface.blit(nearby_bombs, (self._x + self._width * 0.43, (self._y + self._width * 0.43)))
+        if self.get_label() != "flag":
+            font = pygame.font.Font("freesansbold.ttf", 14)
+            nearby_bombs = font.render(str(self.__label), True, (0, 255, 0))
+            surface.blit(nearby_bombs, (self._x + self._width * 0.43, (self._y + self._width * 0.43)))
 
         if self.__bomb:
             pygame.draw.rect(surface, self._color, [self._x+1, self._y+1, self._width-1, self._height-1],)
+
+        elif self.__label == " ":
+            pygame.draw.rect(surface, (100, 100, 100), [self._x + 1, self._y + 1, self._width - 1, self._height - 1], )
+
+        elif self.__label == "flag":
+            pygame.draw.line(surface, (255, 255, 255),
+                             (self._x + 0.25 * self._width, self._y + 0.25 * self._height),
+                             (self._x + 0.25 * self._width, self._y + 0.75 * self._height),
+                             2
+                             )
+            pygame.draw.rect(surface, (255, 0, 0),
+                             [self._x + 0.25 * self._width,
+                              self._y + 0.25 * self._height,
+                              self._width / 2,
+                              self._height / 4])
 
     def get_label(self):
         return self.__label
@@ -49,46 +65,90 @@ class EventManager(object):
         self.__example_cell = example_cell
         self.__field = field
 
-    def get_click_cell(self, event):
+    def get_click_cell(self, event) -> tuple:  # x, y   (2, 4)
         return event.pos[0]//self.__example_cell.get_w(), event.pos[1] //\
                self.__example_cell.get_w()
 
-    def get_surround_bombs(self, event) -> int:
+    def game_over(self, window):
+        run = True
+        while run:
+            window.fill((255, 0, 0))
+            font = pygame.font.Font("freesansbold.ttf", 55)
+            gameover = font.render("GAME OVER", True, (0, 255, 0))
+            for event in pygame.event.get():  # key mapping of the game
+                # print(event)
+                if event.type == pygame.QUIT:
+                    run = False
+            window.blit(gameover,
+                        (len(self.__field[0]) * self.__example_cell.get_w() // 3, len(self.__field) * self.__example_cell.get_w() // 2))
+            pygame.display.update()
+        return False
+
+    def get_surround_bombs(self, x, y) -> int:
         surround_cells = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
-        x, y = self.get_click_cell(event)
         s = 0
         for cell in surround_cells:
             try:
-                try_cell = list(map(sum, list(zip((x, y), cell))))
+                try_cell = list(map(sum, list(zip((x, y), cell))))  # surrounding cell around clicked cell
+                if -1 in try_cell:
+                    raise IndexError
                 if self.__field[try_cell[1]][try_cell[0]].is_bomb():
                     s += 1
             except IndexError:
                 continue
         return s
 
-    def check_events(self) -> bool:
+    def open_cell(self, window, xx, yy):
+        x = xx
+        y = yy
+        cells_to_check = [(x, y)]
+        while True:
+            cells_to_check.pop(cells_to_check.index((x, y)))
+            # print(f"x - {x}, y - {y}")
+            if not self.__field[y][x].is_bomb():  # if not bomb
+                # print(self.get_surround_bombs(x, y))
+                if self.get_surround_bombs(x, y):  # if there are bombs around ...
+                    self.__field[y][x].set_label(self.get_surround_bombs(x, y))
+
+                else:  # case without bombs
+                    self.__field[y][x].set_label(" ")  # current cell change label
+                    surround_cells = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
+                    for cell in surround_cells:
+                        try:
+                            try_cell = tuple(map(sum, list(zip((x, y), cell))))  # [3, 1] example
+                            if -1 in try_cell:  # index should be positive
+                                raise IndexError
+                            # print(try_cell)
+
+                            if self.__field[try_cell[1]][try_cell[0]].get_label() == "":
+                                # print(self.__field[try_cell[1]][try_cell[0]].get_label())
+                                if try_cell not in cells_to_check:
+                                    cells_to_check.append(try_cell)
+
+                        except IndexError:
+                            continue
+            else:
+                return self.game_over(window)
+            if cells_to_check:
+                x, y = cells_to_check[0]
+            else:
+                break
+        return True
+
+    def check_events(self, window) -> bool:
         for event in pygame.event.get():  # key mapping of the game
             # print(event)
             if event.type == pygame.QUIT:
                 return False
             if event.type == pygame.MOUSEBUTTONUP:
                 x, y = self.get_click_cell(event)
-                if not self.__field[y][x].is_bomb():
-                    if self.get_surround_bombs(event):
-                        self.__field[y][x].set_label(self.get_surround_bombs(event))
-            #     except:
-            #         1
-            #         elif event.key == pygame.K_LEFT:
-            #             figure.move_left()
-            #
-            #         elif event.key == pygame.K_DOWN:
-            #             figure.fall()
-            #
-            #         elif event.key == pygame.K_SPACE:
-            #             figure.set_config(figure.rotated_figure())
-
-                # except IndexError:
-                #     pass
+                if event.button == 1:
+                    return self.open_cell(window, x, y)
+                elif event.button == 3:
+                    if self.__field[y][x].get_label() == "":
+                        self.__field[y][x].set_label("flag")
+                    elif self.__field[y][x].get_label() == "flag":
+                        self.__field[y][x].set_label("")
         return True
 
 
